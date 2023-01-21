1339abe0a4e4935cc983176f54a3aaab10326c14
Jeffrey Asilo <jeffreyasilo.gss@gmail.com>
2023-01-21 15:46:22 +0800

    Added Bitwise AND sample

---
+++ bitwise_operator/bitwise_and.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  unsigned int a = 60; // 0011 1100
+  unsigned int b = 13; // 0000 1101
+  int result = 0; 
+
+  // You evaluate each bit position with AND (Just like how the AND works in any programming language) e.g. 0 & 1 = 0
+
+  result = a & b; // 0000 1100 or 12
+
+  printf("Total Result: %d", result);
+}
